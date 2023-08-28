327b5ea0e7f8cbc7e9030ab1d0ebb02dcf59b08e
yaswanth <113416864+yaswanth7842@users.noreply.github.com>
2023-08-28 07:36:18 +0530

    Create 12 Tictoe Game.py

---
+++ 12 Tictoe Game.py
@@ -0,0 +1,48 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for i in range(3):
+        if all([board[i][j] == player for j in range(3)]) or all([board[j][i] == player for j in range(3)]):
+            return True
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
+
+def is_board_full(board):
+    return all([cell != " " for row in board for cell in row])
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    turn = 0
+
+    while True:
+        print_board(board)
+        player = players[turn % 2]
+        print(f"Player {player}'s turn")
+
+        row = int(input("Enter row (0, 1, or 2): "))
+        col = int(input("Enter column (0, 1, or 2): "))
+
+        if row < 0 or row > 2 or col < 0 or col > 2 or board[row][col] != " ":
+            print("Invalid move. Try again.")
+            continue
+
+        board[row][col] = player
+
+        if check_winner(board, player):
+            print_board(board)
+            print(f"Player {player} wins!")
+            break
+        elif is_board_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        turn += 1
+
+if __name__ == "__main__":
+    main()
